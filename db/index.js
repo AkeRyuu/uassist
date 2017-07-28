c71cc16842d833416f19bce0a8e51af8964ec5ac
AkeRyuu <berezhnogo@gmail.com>
2017-07-28 10:40:07 +0300

    add pug

---
+++ db/index.js
@@ -0,0 +1,82 @@
+var table_items = [{
+        title: "Ppa per hour",
+        desc: "The Roosevelt dime is the current ten-cent piece of the United States, displaying President Franklin D. Roosevelt on theobverse.",
+        rate_item: ["Different hour rate for different services"],
+        btn: "Contact us"
+    },
+    {
+        title: "Traveller package",
+        desc: "The Roosevelt dime is the current ten-cent piece of the United States, displaying President Franklin D. Roosevelt on theobverse.",
+        rate_item: ["up to 4h", "up to 8h"],
+        btn: "Book now"
+    },
+    {
+        title: "Research package",
+        desc: "The Roosevelt dime is the current ten-cent piece of the United States, displaying President Franklin D. Roosevelt on theobverse.",
+        rate_item: ["up to 4h", "up to 8h"],
+        btn: "Book now"
+    },
+    {
+        title: "Business package",
+        desc: "The Roosevelt dime is the current ten-cent piece of the United States, displaying President Franklin D. Roosevelt on theobverse.",
+        rate_item: ["up to 4h", "up to 8h"],
+        btn: "Book now"
+    },
+    {
+        title: "Individual project",
+        desc: "The Roosevelt dime is the current ten-cent piece of the United States, displaying President Franklin D. Roosevelt on theobverse.",
+        rate_item: ["Contact us and we'll discuss your project"],
+        btn: "Contact us"
+    }
+];
+var menu_list = [{
+        name: "Home",
+        id: "video-area"
+    },
+    {
+        name: "Services",
+        id: "services"
+    },
+    {
+        name: "How?",
+        id: "features"
+    }, {
+        name: "Packages",
+        id: "pricing"
+    }, {
+        name: "Contact",
+        id: "contact"
+    }, {
+        name: "subscribe",
+        id: "Subscribe"
+    }
+]
+
+var service_list = [{
+        name: "Be your local assistant",
+        desc: "Uassist provides call-center services 9-20h for saving your time for the most important things. Be the local secretary"
+    },
+    {
+        name: "Provide a research",
+        desc: "To be a local fixer. Giving you an access to local databases "
+    },
+    {
+        name: "Be the local guide ",
+        desc: "Provide local personal tours. Helping you to see Lviv from the best perspective"
+    },
+    {
+        name: "Connect you to business opportunities",
+        desc: "Be the local business advisor and partner. Local trust agent. Help with booking meetings and identifying potential business partners. Market research on basic economic data, basic feasibility of their products, to know aboutlegal assistance they can get."
+    },
+    {
+        name: "Help you find local contacts",
+        desc: "Help you find who prominent people in your discipline, influential political decision-makers, other research institutions are on location, to arrange meetings with subject-matter-experts who are in town"
+    },
+    {
+        name: "Help to set up a company in Ukraine",
+        desc: "The issues during registration of a company in Ukraine is the complexity of the procedure, mistakes in processing documents, time consuming procedure"
+    }
+]
+module.exports.table_items = table_items;
+module.exports.menu_list = menu_list;
+module.exports.service_list = service_list;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var db = require("./../db");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(db.table_items);
+  res.render('index', { title: 'Express',table_items : db.table_items,menu:db.menu_list,services:db.service_list });
 });
 
 module.exports = router;
